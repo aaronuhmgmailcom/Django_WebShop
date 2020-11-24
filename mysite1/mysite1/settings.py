035a54cf3002be9c318237e53035844c9ba9e969
peter <54778723@qq.com>
2020-11-24 09:34:11 +0800

    init project

--- mysite1/mysite1/settings.py
+++ mysite1/mysite1/settings.py
@@ -146,6 +146,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' # 固定写法
 # EMAIL_HOST = 'smtp.qq.com' # 腾讯QQ邮箱 SMTP 服务器地址
 # EMAIL_PORT = 25  # SMTP服务的端口号
-# EMAIL_HOST_USER = '5477872@qq.com'  # 发送邮件的QQ邮箱
+# EMAIL_HOST_USER = '**@qq.com'  # 发送邮件的QQ邮箱
 # EMAIL_HOST_PASSWORD = 'qyijsjqyobdrvbhgdwe'  # 在QQ邮箱->设置->帐户->“POP3/IMAP......服务” 里得到的在第三方登录QQ邮箱授权码
 # EMAIL_USE_TLS = True  # 与SMTP服务器通信时，是否启动TLS链接(安全链接)默认false
